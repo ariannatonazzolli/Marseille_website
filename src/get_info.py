f87a33fe600b3b6b3ad9381f15e5f1f2b37f9c73
Arianna Tonazzolli <arianna.tonazzolli@gmail.com>
2024-12-19 23:06:54 +0100

    Retrieve quarter and streets names

--- src/get_info.py
+++ src/get_info.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "https://www.marseille.fr/logement-urbanisme/am%C3%A9lioration-de-lhabitat/arretes-de-peril" 
 response = requests.get(url)
@@ -7,9 +8,19 @@ response = requests.get(url)
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Extract specific elements
-titles = soup.find_all(['p'])
+def clean_text(text):
+    text = re.sub(r'\u200B', '', text) # Remove zero-width space characters (Unicode \u200B)
+    text = text.strip()  # Remove leading and trailing whitespace
+    return text
 
-# Step 5: Print out the titles
-for title in titles:
-    print(title.get_text())
+# Extract the quarters and streets names
+quarters = soup.find_all('dt')
+for quarter in quarters:
+    all_streets_quarter = quarter.find_next_sibling('dd')
+    streets = all_streets_quarter.find_all('p')
+    print('\n', quarter.get_text())
+    for street in streets:
+        street_name = street.get_text().strip()
+        if street_name and not street_name.startswith('-'):
+            print(clean_text(street_name))
+
